c89abc0e5d779b3e030efd0879f3591fe7d81339
Diogo Carneiro <diogoascarneiro@hotmail.com>
2021-12-07 15:28:30 +0000

    added first cars and roads

--- js/main.js
+++ js/main.js
@@ -5,13 +5,19 @@ const ctx = canvas.getContext("2d");
  let lola = new Dog(10, 10, 50, 50);
  let animationID = null;
  let gameOver = false;
+ let cars = [];
+ let carFrequency = 0;
 
 
  /* drop testing stuff here*/
- let testObst = new Obstacle(100, 100, 50, 50);
+ let testObst = new Obstacle(100, 100, 50, 50, "red");
+ let testRoad = new Road(500, 50);
+ let testCar = new Car(250, 525, 10, 10, "purple");
+ cars.push(testCar);
+ createCar();
+
  window.addEventListener('load', (event) => {
-    testObst.drawObstacle();
-    lola.drawDog();
+    updateEverything();
   });
  
 
@@ -28,13 +34,36 @@ function updateEverything() {
     clearCanvas();
     lola.drawDog();
     testObst.drawObstacle();
-    
+    testRoad.drawObstacle();
+     
+   /*
+   
+   Isto abaixo gera um array cada vez maior. Pode ser fonte de problemas?
+
+   carFrequency++;
+if (carFrequency % 200 === 1) {
+  createCar();
+}
+
+*/
+
+    cars.forEach((car) => {
+      if (car.x < canvas.width) {
+      car.moveRight();
+      car.drawObstacle();
+    } else {
+      car.x = 0;
+    }
+
+      gameOver = detectCollision(car);
+    });
+
    if (!gameOver) {
     animationID = requestAnimationFrame(updateEverything);
   } else {
     cancelAnimationFrame(animationID);
     clearCanvas();
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "black";
     ctx.font = "20px Georgia";
     ctx.fillText("Oh noes! Game Over!", 50, 200);
   }
@@ -42,7 +71,6 @@ function updateEverything() {
 
   document.addEventListener("keydown", (keyboardEvent) => {
     lola.move(keyboardEvent.key);
-    updateEverything();
-  });
+    });
 
  
--- js/obstacles.js
+++ js/obstacles.js
@@ -1,31 +1,62 @@
+/*
+* Preciso de pensar aqui um bocado nos diferentes tipos de obstaculo. Estrada, pessoas (boas e más)?
+*
+*
+*
+*/
+
 class Obstacle {
-    constructor(x, y, width, height) {
+    constructor(x, y, width, height, color) {
       this.x = x;
       this.y = y;
       this.dx = 4;
       this.dy = 4;
       this.width = width;
       this.height = height;
+      this.color = color;
       }
   
     drawObstacle() {
-      ctx.fillStyle = "red";
+      ctx.fillStyle = this.color;
       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-  
-    moveLeft() {
-        this.x -= this.dx;
-      }
-      moveRight() {
-        this.x += this.dx;
-      }
-      moveUp() {
-        this.y -= this.dy;
-      }
-      moveDown() {
-        this.y += this.dy;
+ }
+
+  class Road extends Obstacle {
+    constructor(y, height) {
+       super(0, y, canvas.width, height, "lightgray");
       }
 
+  }
+
+  class Car extends Obstacle {
+  
+ moveLeft() {
+  if (this.x > 0) {
+    this.x -= this.dx;
+     }
+}
+moveRight() {
+  if (this.x < canvas.width) {
+    this.x += this.dx;
+  } 
+}
+
+moveUp() {
+ if (this.y > 0) {
+   this.y -= this.dy;
+    }
+}
+moveDown() {
+  if (this.y < canvas.height) {
+    this.y += this.dy;
+  }
+}
+  }
+
+  function createCar() {
+    let newCar = new Car(0, 525, 10, 10, "purple");
+    cars.push(newCar);
   }
 
   /*leaving it only for player vs obstacles for now but would like to to obstacle on obstacle in the future, maybe*/
